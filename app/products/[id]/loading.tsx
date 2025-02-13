7d28da83d168e35f49d3acfc4897fcd17888dfaa
Walid Elsergany <125218561+WalidElsergany74@users.noreply.github.com>
2025-02-13 15:33:50 +0200

    Update loading.tsx

--- app/products/[id]/loading.tsx
+++ app/products/[id]/loading.tsx
@@ -1,38 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const loading = () => {
+const SkeletonCard = () => {
   return (
-    <div className="relative flex flex-col items-start w-full animate-pulse">
-      {/* Image Skeleton */}
-      <div className="relative w-full h-64 bg-gray-200 rounded-md"></div>
+    <div className="w-full h-40 bg-gray-200 rounded-md"></div>
+  );
+};
 
-      {/* Details Skeleton */}
-      <div className="flex flex-col w-full mt-2 space-y-3">
-        {/* Title Skeleton */}
-        <div className="w-3/4 h-4 bg-gray-200 rounded-md"></div>
+const SkeletonLoader = () => {
+  return (
+    <div className="flex w-full h-screen animate-pulse p-4">
+      <div className="hidden md:block w-1/4 h-full bg-gray-200 rounded-md"></div>
 
-        {/* Color and Rating Skeleton */}
-        <div className="flex items-center gap-2 mt-1">
-          <div className="w-16 h-4 bg-gray-200 rounded-md"></div>
-          <div className="flex space-x-1">
-            {Array.from({ length: 5 }, (_, index) => (
-              <div key={index} className="w-4 h-4 bg-gray-200 rounded-md"></div>
-            ))}
-          </div>
+      {/* Main Content */}
+      <div className="flex flex-col flex-1 px-4 space-y-4">
+        {/* Image Skeleton */}
+        <div className="w-full h-64 bg-gray-300 rounded-md"></div>
+
+        {/* Grid Skeleton */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {Array.from({ length: 6 }).map((_, index) => (
+            <SkeletonCard key={index} />
+          ))}
         </div>
-
-        {/* Price and Discount Skeleton */}
-        <div className="mt-2 flex items-center space-x-2">
-          <div className="w-16 h-4 bg-gray-200 rounded-md"></div>
-          <div className="w-10 h-4 bg-gray-200 rounded-md"></div>
-          <div className="w-12 h-4 bg-gray-200 rounded-md"></div>
-        </div>
-
-        {/* Add Button Skeleton */}
-        <div className="mt-2 w-full h-10 bg-gray-300 rounded-md"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default loading
+export default SkeletonLoader;
