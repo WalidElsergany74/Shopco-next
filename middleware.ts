60a5bcb7971e7fba7e6db8ac4e25d199f5dc6526
Walid Elsergany <125218561+WalidElsergany74@users.noreply.github.com>
2025-02-13 14:01:19 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/admin(.*)', '/forum(.*)'])
+const isProtectedRoute = createRouteMatcher(['/admin(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = auth()
